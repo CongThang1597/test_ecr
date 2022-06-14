e952498df2d58f639983fa65b70e245675627575
CongThang <thangnc.mta@gmail.com>
2022-06-14 15:51:18 +0700

    build

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
+
+
+@app.route('/health')
+def hello_geek():
+    return 'success'
 
 
 if __name__ == "__main__":
